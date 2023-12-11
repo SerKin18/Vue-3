a5ceaa2c2bebe28918220c9d2d8b8f3459e1811f
Sergey <sergeykinyak@gmail.com>
2023-12-11 21:41:26 +0200

    fetch posts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="app">
-	<h1>Страница с постами</h1>
-	<my-button
-	style="margin:15px 0px "
-	@click="showDialog">Создать пользователя</my-button>
+    <h1>Страница с постами</h1>
+    <my-button style="margin: 15px 0px" @click="showDialog"
+      >Создать пользователя</my-button
+    >
     <my-dialog v-model:show="dialogVisible">
       <post-form @create="createPost" />
     </my-dialog>
 
-    <post-list :posts="posts" @remove="removePost" />
+    <post-list :posts="posts" @remove="removePost" v-if="!isPostsLoading" />
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 <script>
 import PostForm from "./components/PostForm";
 import PostList from "@/components/PostList";
-
+import axios from "axios";
 export default {
   components: {
     PostForm,
@@ -23,26 +24,42 @@ export default {
 
   data() {
     return {
-      posts: [
-        { id: 1, title: "JavaScript", body: "описание поста" },
-        { id: 2, title: "JavaScript 2", body: "описание поста 2" },
-        { id: 3, title: "JavaScript 3", body: "описание поста 3" },
-        { id: 4, title: "JavaScript 4", body: "описание поста 4" },
-      ],
-		dialogVisible:false
+      posts: [],
+      dialogVisible: false,
+      isPostsLoading: false,
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
-		this.dialogVisible=false;
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
     },
-	 showDialog(){
-		this.dialogVisible=true;
-	 }
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        setTimeout(async () => {
+          const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          );
+          this.isPostsLoading = false;
+          console.log(response);
+          this.posts = response.data;
+        }, 1000);
+      } catch (err) {
+        alert("Ошибка");
+      } finally {
+        //   this.isPostsLoading = false;
+      }
+    },
+  },
+  mounted() {
+    this.fetchPosts();
   },
 };
 </script>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div  v-if="posts.length > 0">
-    <h3>Список пользователей</h3>
+    <h3>Список постов</h3>
     <post-item
       v-for="post in posts"
       :post="post"
@@ -9,7 +9,7 @@
     />
   </div>
   <h2 v-else style ="color:red">
-Список пользователей пуст
+Список постов пуст
   </h2>
 </template>
 <script>
